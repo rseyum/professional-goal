8907857802017b39e09061e5c3bdd541003a1729
codewithrich <103169939+rseyum@users.noreply.github.com>
2022-08-17 05:51:26 +0000

    updates-bio

--- index.js
+++ index.js
@@ -25,10 +25,27 @@ app.get('/Skills', (req, res) => {
     'heading':heading
   })
 })
+
 app.get('/Education', (req, res) => {
   var title = "Year Up Application Dev!"
   var heading = 'Year Up '
   res.render('pages/education',{
+    'title':title,
+    'heading':heading
+  })
+})
+app.get('/Hobbies', (req, res) => {
+  var title = "My Hobbies!"
+  var heading = 'Top Hobbies'
+  res.render('pages/hobbies',{
+    'title':title,
+    'heading':heading
+  })
+})
+app.get('/work-experience', (req, res) => {
+  var title = "My work Experience!"
+  var heading = 'Work history'
+  res.render('pages/work-experience',{
     'title':title,
     'heading':heading
   })
