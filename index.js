e7deaa46ed8c3002ff8e2e904fa0f0e90b929ae3
codewithrich <103169939+rseyum@users.noreply.github.com>
2022-08-17 06:19:03 +0000

    updates-profile

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
   var title = "Welcome to my Page!"
-  var heading = "Rahel Seyum Year Up student and Front-end development passioate "
+  var heading = "Rahel Seyum Year Up student!"
   res.render('pages/index',{
     'title':title,
     'heading':heading
